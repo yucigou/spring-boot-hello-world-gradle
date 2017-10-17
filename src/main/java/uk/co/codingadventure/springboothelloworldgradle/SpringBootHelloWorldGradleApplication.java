5d8b076babbe23905a5d22e13775934f9bf15890
Yuci Gou <ygou@ebi.ac.uk>
2017-10-17 12:12:22 +0100

    Initial commit

---
+++ src/main/java/uk/co/codingadventure/springboothelloworldgradle/SpringBootHelloWorldGradleApplication.java
@@ -0,0 +1,12 @@
+package uk.co.codingadventure.springboothelloworldgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldGradleApplication.class, args);
+	}
+}
---
+++ src/test/java/uk/co/codingadventure/springboothelloworldgradle/SpringBootHelloWorldGradleApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.co.codingadventure.springboothelloworldgradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootHelloWorldGradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
